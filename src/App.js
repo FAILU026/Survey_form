c9e39f997affa995cf382527ac4c81e63afa6f42
FAILU026 <failurrahumanjpm@gmail.com>
2023-11-27 12:39:17 +0530

    add frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState,useEffect } from 'react'
+import "./App.css";
+import axios from 'axios';
+import {  Routes, Route} from "react-router-dom";
+import Data from './Components/Data';
+import Nav from './Components/Nav';
+import Form from './Components/Form';
+import Home from './Components/Home';
 
-function App() {
+
+const App = () => {
+
+  const [formData, setFormData] = useState({
+    name: '',
+    gender: '',
+    nationality: '',
+    email: '',
+    phoneNumber: '',
+    address: '',
+    message: '',
+  });
+
+  
+  const handleSubmit = async (e)=>{
+    e.preventDefault()
+    const data = await  axios.post('http://localhost:5000/create',formData)
+    console.log(data)
+    if(data.data.success){
+      alert("data Save successfully")}
+      
+  }
+
+  const [dataList,setDataList]= useState([])
+
+   
+    axios.get('http://localhost:5000/read')
+          .then(response => setDataList(response.data))
+          .catch(error => console.error('Error fetching surveys:', error));
+  
+      
+  useEffect(() => {
+    axios.get('http://localhost:5000/read')
+          .then(response => setDataList(response.data))
+          .catch(error => console.error('Error fetching surveys:', error));
+        },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    
+    <Nav />
+    <Routes>
+
+        <Route path='/' element={<Home/>} />
+        <Route path='/form' element={<Form
+            handleSubmit={handleSubmit} 
+            formData={formData}
+            setFormData={setFormData}
+        />} />
+        <Route path='/data' element={< Data 
+            dataList={dataList}
+        
+        />} />
+  </Routes>
+
+
+
+    </>
+  )
 }
 
-export default App;
+export default App
